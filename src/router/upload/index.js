e8cfd6c95ebaccf387bf7befd2683ee31f44d036
Du Cheng <easyhoo2426@163.com>
2022-05-10 14:06:14 +0800

    feat: upload 上传文件接口

--- src/router/upload/index.js
+++ src/router/upload/index.js
@@ -1,11 +1,34 @@
 const Router = require('koa-router')
 
-const { verifyToken, verifyPermission } = require('../../common/common-middleware')
-const { saveImg, handleAvatar, handlePictures, resizeFile } = require('./middleware')
+const {
+	verifyToken,
+	verifyPermission,
+} = require('../../common/common-middleware')
 
-const uploadRouter = new Router({prefix: '/upload'})
+const {
+	saveImg,
+	handleAvatar,
+	handlePictures,
+	resizeFile,
+} = require('./middleware')
 
-uploadRouter.post('/avatar', verifyToken, saveImg('avatar'), resizeFile, handleAvatar)  // 上传头像
-uploadRouter.post('/:momentId/picture', verifyToken, verifyPermission("moment"), saveImg('picture'), resizeFile, handlePictures)  // 上传动态配图
+const uploadRouter = new Router({ prefix: '/upload' })
+
+uploadRouter.post(
+	'/avatar',
+	verifyToken,
+	saveImg('avatar'),
+	resizeFile,
+	handleAvatar
+) // 上传头像
+
+uploadRouter.post(
+	'/:momentId/picture',
+	verifyToken,
+	verifyPermission('moment'),
+	saveImg('picture'),
+	resizeFile,
+	handlePictures
+) // 上传动态配图
 
 module.exports = uploadRouter
