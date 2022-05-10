3108a69af088ba48ab1ae054247c7908349358ba
Du Cheng <easyhoo2426@163.com>
2022-05-10 13:37:39 +0800

    feat(comment): 点赞接口完成

--- src/router/comment/index.js
+++ src/router/comment/index.js
@@ -1,15 +1,41 @@
 const Router = require('koa-router')
 
-const { verifyToken, verifyPermission, verifyMoment, verifyMomentAndComment } = require('../../common/common-middleware')
-const { pubComment, replyComment, deleteComment, commentList, goAgree, commentDetailList } = require('./middleware')
+const {
+	verifyToken,
+	verifyPermission,
+	verifyMoment,
+	verifyMomentAndComment,
+} = require('../../common/common-middleware')
+const {
+	pubComment,
+	replyComment,
+	deleteComment,
+	commentList,
+	goAgree,
+	commentDetailList,
+} = require('./middleware')
 
-const commentRouter = new Router({prefix: '/comment'})
+const commentRouter = new Router({ prefix: '/comment' })
 
 commentRouter.post('/', verifyToken, verifyMoment, pubComment) // 对动态发表评论
-commentRouter.post('/:commentId', verifyToken, verifyMomentAndComment, replyComment) // 对评论进行回复
-commentRouter.delete('/:commentId', verifyToken, verifyPermission("comment"), deleteComment) // 删除评论
+
+commentRouter.post(
+	'/:commentId',
+	verifyToken,
+	verifyMomentAndComment,
+	replyComment
+) // 对评论进行回复
+
+commentRouter.delete(
+	'/:commentId',
+	verifyToken,
+	verifyPermission('comment'),
+	deleteComment
+) // 删除评论
+
 commentRouter.get('/', commentList) // 获取动态的评论列表
 // commentRouter.get('/:commentId', commentDetailList) // 获取动态某个评论的回复列表
+
 commentRouter.post('/:commentId/like', verifyToken, goAgree) // 点赞
 
 module.exports = commentRouter
--- src/router/comment/service.js
+++ src/router/comment/service.js
@@ -1,39 +1,41 @@
 const connection = require('../../app/database')
 
 class CommentService {
-  // 发表评论
-  async pub(uId, content, mId) {
-    const statement = "INSERT INTO `comment` (user_id, content, moment_id) VALUES (?, ?, ?)"
-    try {
-      await connection.execute(statement, [uId, content, mId])
-      return "发表评论成功"
-    } catch (error) {
-      return "发表评论失败"
-    }
-  }
+	// 发表评论
+	async pub(uId, content, mId) {
+		const statement =
+			'INSERT INTO `comment` (user_id, content, moment_id) VALUES (?, ?, ?)'
+		try {
+			await connection.execute(statement, [uId, content, mId])
+			return '发表评论成功'
+		} catch (error) {
+			return '发表评论失败'
+		}
+	}
 
-  // 回复评论
-  async reply(uId, content, mId, cId) {
-    const statement = "INSERT INTO comment (user_id, content, moment_id, comment_id) VALUES (?, ?, ?, ?)"
-    const result = await connection.execute(statement, [uId, content, mId, cId])
+	// 回复评论
+	async reply(uId, content, mId, cId) {
+		const statement =
+			'INSERT INTO comment (user_id, content, moment_id, comment_id) VALUES (?, ?, ?, ?)'
+		const result = await connection.execute(statement, [uId, content, mId, cId])
 
-    return result[0]
-  }
+		return result[0]
+	}
 
-  // 删除回复
-  async remove(id) {
-    const statement = "DELETE FROM comment WHERE id = ?"
-    try {
-      await connection.execute(statement, [id])
-      return "删除成功"
-    } catch (error) {
-      return "删除失败" + error.message
-    }
-  }
+	// 删除回复
+	async remove(id) {
+		const statement = 'DELETE FROM comment WHERE id = ?'
+		try {
+			await connection.execute(statement, [id])
+			return '删除成功'
+		} catch (error) {
+			return '删除失败' + error.message
+		}
+	}
 
-  // 根据动态获取该动态的评论列表
-  async listInMoment(userId, momentId, order, offset, limit) {
-    const statement = `
+	// 根据动态获取该动态的评论列表
+	async listInMoment(userId, momentId, order, offset, limit) {
+		const statement = `
       SELECT c.id, c.content, c.createTime, c.moment_id momentId, c.comment_id commentId,
         JSON_OBJECT('id', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url) user,
         (SELECT COUNT(*) FROM comment_agree cg WHERE cg.comment_id = c.id) agree,
@@ -44,17 +46,22 @@ class CommentService {
       ORDER BY ${order} DESC
       LIMIT ?, ?
     `
-    try {
-      const [result] = await connection.execute(statement, [userId, momentId, offset, limit])
-      return result
-    } catch (error) {
-      return error.message
-    }
-  }
+		try {
+			const [result] = await connection.execute(statement, [
+				userId,
+				momentId,
+				offset,
+				limit,
+			])
+			return result
+		} catch (error) {
+			return error.message
+		}
+	}
 
-  // 获取动态某个评论的回复列表
-  async listInComment(userId, commentId) {
-    const statement = `
+	// 获取动态某个评论的回复列表
+	async listInComment(userId, commentId) {
+		const statement = `
       SELECT c.id, c.content, c.createTime, c.moment_id momentId, c.comment_id commentId,
         JSON_OBJECT('id', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url) user,
         (SELECT COUNT(*) FROM comment_agree cg WHERE cg.comment_id = c.id) agree,
@@ -63,22 +70,22 @@ class CommentService {
       WHERE c.comment_id = ?
       ORDER BY c.createTime ASC
     `
-    try {
-      const [result] = await connection.execute(statement, [userId, commentId])
-      return result
-    } catch (error) {
-      return error.message
-    }
-  }
+		try {
+			const [result] = await connection.execute(statement, [userId, commentId])
+			return result
+		} catch (error) {
+			return error.message
+		}
+	}
 
-  // 根据用户id获取评论列表
-  async listInUser(userId, offset, limit) {
-    // SELECT c.id, c.content, c.createTime, c.moment_id momentId, c.comment_id commentId
-    //   FROM comment c
-    //   WHERE c.user_id = ?
-    //   ORDER BY c.createTime DESC
-    //   LIMIT ?, ?
-    const statement = `
+	// 根据用户id获取评论列表
+	async listInUser(userId, offset, limit) {
+		// SELECT c.id, c.content, c.createTime, c.moment_id momentId, c.comment_id commentId
+		//   FROM comment c
+		//   WHERE c.user_id = ?
+		//   ORDER BY c.createTime DESC
+		//   LIMIT ?, ?
+		const statement = `
       SELECT c.id, c.content, c.createTime, c.moment_id momentId, c.comment_id commentId,
         JSON_OBJECT('id', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url) user,
         (SELECT COUNT(*) FROM comment_agree cg WHERE cg.comment_id = c.id) agree,
@@ -89,13 +96,18 @@ class CommentService {
       ORDER BY c.createTime DESC
       LIMIT ?, ?
     `
-    try {
-      const [res] = await connection.execute(statement, [userId, userId, offset, limit])
-      return res
-    } catch (error) {
-      return error.message
-    }
-  }
+		try {
+			const [res] = await connection.execute(statement, [
+				userId,
+				userId,
+				offset,
+				limit,
+			])
+			return res
+		} catch (error) {
+			return error.message
+		}
+	}
 }
 
 module.exports = new CommentService()
