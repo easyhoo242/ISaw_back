94e6f5f2df3014a81da4127ec87110318b632771
Du Cheng <easyhoo2426@163.com>
2022-06-05 13:46:03 +0800

    fix: 将所有状态码都改成200

--- src/util/error-handle.js
+++ src/util/error-handle.js
@@ -4,69 +4,69 @@ const { ErrResult } = require('../app/responseInfo')
 const errorHandle = (err, ctx) => {
 	switch (err.message) {
 		case errorType.USERNAME_PASSWORD_IS_NULL:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('账号或密码或昵称不能为空！')
 			break
 
 		case errorType.USERNAME_EXIST:
-			ctx.status = 409
+			ctx.status = 200
 			ctx.body = new ErrResult('账号已存在')
 			break
 
 		case errorType.NICKNAME_EXIST:
-			ctx.status = 409
+			ctx.status = 200
 			ctx.body = new ErrResult('该昵称已存在')
 			break
 
 		case errorType.USERNAME_PASSWORD_RULE:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult(
 				'账号或密码不符合规则（账号：4-16 字母或数字，密码：4-16 字母数字符号至少包含2种）'
 			)
 			break
 
 		case errorType.USERNAME_IS_NULL:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('账号不存在')
 			break
 
 		case errorType.USERNAME_PASSWORD_ERROR:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('密码错误')
 			break
 
 		case errorType.TOKEN_NOT_OK:
-			ctx.status = 401
+			ctx.status = 200
 			ctx.body = new ErrResult('未登录')
 			break
 
 		case errorType.CONTENT:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('content内容为空或内容过长')
 			break
 
 		case errorType.NOT_OPERATION_PERMISSION:
-			ctx.status = 401
+			ctx.status = 200
 			ctx.body = new ErrResult('没有操作权限')
 			break
 
 		case errorType.PARAMS_ERROR:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('参数错误')
 			break
 
 		case errorType.MOMENT_EXIST:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('该动态不存在')
 			break
 
 		case errorType.COMMENT_EXIST:
-			ctx.status = 400
+			ctx.status = 200
 			ctx.body = new ErrResult('该评论不存在')
 			break
 
 		default:
-			ctx.status = 404
+			ctx.status = 200
 			ctx.body = 'Not Found'
 	}
 }
